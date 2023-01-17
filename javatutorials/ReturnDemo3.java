a5153d79f6d2ee90dff434ab2c328dec52528e1a
박민주 <parkminjoo@bagminjuui-MacBookPro.local>
2023-01-17 11:26:42 +0700

    [UPDATE] ReturnDemo4

---
+++ javatutorials/ReturnDemo3.java
@@ -0,0 +1,21 @@
+package com.example.javatutorials;
+
+public class ReturnDemo3 {
+    public static String getMember1() {
+        return "Minjoo";
+    }
+
+    public static String getMember2() {
+        return "Euijin";
+    }
+
+    public static String getMember3() {
+        return "David";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMember1());
+        System.out.println(getMember2());
+        System.out.println(getMember3());
+    }
+}
