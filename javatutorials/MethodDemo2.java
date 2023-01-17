b46b59aa9b34ccb8883fdc9b84e6dd304f4579b2
박민주 <parkminjoo@bagminjuui-MacBookPro.local>
2023-01-17 10:45:35 +0700

    [UPDATE] MethodDemo2

---
+++ javatutorials/MethodDemo2.java
@@ -0,0 +1,19 @@
+package com.example.javatutorials;
+
+public class MethodDemo2 {
+    public static void numbering() {
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        numbering();
+        numbering();
+        numbering();
+        numbering();
+
+    }
+}
