2adea735f2e62bb53156943c06fe02ea8abaaec7
박민주 <parkminjoo@bagminjuui-MacBookPro.local>
2023-01-17 11:20:27 +0700

    [UPDATE] ReturnDemo2

--- javatutorials/ReturnDemo.java
+++ javatutorials/ReturnDemo.java
@@ -3,8 +3,8 @@ package com.example.javatutorials;
 public class ReturnDemo {
     public static int one() {
         return 1;
-        return 2;
-        return 3;
+//        return 2;
+//        return 3;
     }
 
     public static void main(String[] args) {
