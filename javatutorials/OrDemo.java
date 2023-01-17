a2d4598b8bd3269ed7c92beacd180635d3854dfd
박민주 <parkminjoo@bagminjuui-MacBookPro.local>
2023-01-17 09:33:24 +0700

    [UPDATE] OrDemo

---
+++ javatutorials/OrDemo.java
@@ -0,0 +1,18 @@
+package com.example.javatutorials;
+
+public class OrDemo {
+    public static void main(String[] args) {
+        if (true || true) {
+            System.out.println(1);
+        }
+        if (true || false) {
+            System.out.println(2);
+        }
+        if (false || true) {
+            System.out.println(3);
+        }
+        if (false || false) {
+            System.out.println(4);
+        }
+    }
+}
