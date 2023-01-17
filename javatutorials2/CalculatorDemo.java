d5dc9136ff4c5bdf65f258088d0bee197f39bf19
박민주 <parkminjoo@bagminjuui-MacBookPro.local>
2023-01-17 14:07:54 +0700

    [UPLOAD] Java Tutorials2

---
+++ javatutorials2/CalculatorDemo.java
@@ -0,0 +1,11 @@
+public class CalculatorDemo {
+    public static void sum(int left, int right) {
+        System.out.println(left + right);
+    }
+    public static void main(String[] args) {
+//        System.out.println(10 + 20);
+//        System.out.println(20 + 40);
+        sum(10, 20);
+        sum(20, 40);
+    }
+}
