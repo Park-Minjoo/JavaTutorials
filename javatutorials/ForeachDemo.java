806009816f90ccee319e4da7fac6eda2a9273e25
박민주 <parkminjoo@bagminjuui-MacBookPro.local>
2023-01-17 10:24:43 +0700

    [UPDATE] ForeachDemo

---
+++ javatutorials/ForeachDemo.java
@@ -0,0 +1,10 @@
+package com.example.javatutorials;
+
+public class ForeachDemo {
+    public static void main(String[] args) {
+        String[] members = {"Azaria", "Rafin", "Daniel"};
+        for (String e : members) {
+            System.out.println(e + " must study harder...");
+        }
+    }
+}
