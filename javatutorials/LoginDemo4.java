9dad7edffbc04eceff25895fc974ba818c3924db
박민주 <parkminjoo@bagminjuui-MacBookPro.local>
2023-01-17 09:38:06 +0700

    [UPDATE] LoginDemo4.java

---
+++ javatutorials/LoginDemo4.java
@@ -0,0 +1,14 @@
+package com.example.javatutorials;
+
+public class LoginDemo4 {
+    public static void main(String[] args) {
+        String id = args[0];
+        String password = args[1];
+        if ((id.equals("minjoo") || id.equals("hey930") || id.equals("daniel"))
+                && password.equals("1234")) {
+            System.out.println("right");
+        } else {
+            System.out.println("wrong");
+        }
+    }
+}
