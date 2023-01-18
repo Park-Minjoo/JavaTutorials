bdea64795784edacd01361b00d8cf0d3e8e51a54
박민주 <parkminjoo@bagminjuui-MacBookPro.local>
2023-01-18 16:09:38 +0700

    [UPLOAD] ScopeDemo4

---
+++ javatutorials2/ScopeDemo4.java
@@ -0,0 +1,11 @@
+public class ScopeDemo4 {
+    static void a() {
+        String title = "Hello JIU students";
+        // regional variable -> access in method
+    }
+    public static void main(String[] args){
+        a();
+//        System.out.println(title);
+        // it doesn't work...
+    }
+}
