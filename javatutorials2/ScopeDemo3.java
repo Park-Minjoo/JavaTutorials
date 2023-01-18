63db1467634a7c8e9d971ff895d4e60ce14437ec
박민주 <parkminjoo@bagminjuui-MacBookPro.local>
2023-01-18 11:55:43 +0700

    [UPDATE] ScpeDemo3

--- javatutorials2/ScopeDemo3.java
+++ javatutorials2/ScopeDemo3.java
@@ -3,6 +3,12 @@ public class ScopeDemo3 {
 
     static void a() {
         int i = 0;
+        // In terms of method alone
+        //   -> the variable declared in the method is created
+        //      when the method is executed and
+        //      deleted when the method is terminated
+        // the variable in class <<< the variable in method
+        // The principle that regional things have higher priorities than global things
     }
 
     public static void main(String[] args){
