283144308d28c1f59e42381aa3db15b2eb41b8db
박민주 <parkminjoo@bagminjuui-MacBookPro.local>
2023-01-18 11:34:24 +0700

    [UPDATE] ScopeDemo2

---
+++ javatutorials2/ScopeDemo2.java
@@ -0,0 +1,14 @@
+public class ScopeDemo2 {
+    static int i;
+
+    static void a() {
+        i = 0;
+    }
+
+    public static void main(String[] args) {
+        for (i = 0; i < 5; i++) {
+            a();
+            System.out.println(i);
+        }
+    }
+}
