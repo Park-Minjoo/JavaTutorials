520b218a85ef76fcc3dfc1bf892e766847d37294
박민주 <parkminjoo@bagminjuui-MacBookPro.local>
2023-01-18 16:29:02 +0700

    [UPDATE] ScopeDemo6

---
+++ javatutorials2/ScopeDemo5.java
@@ -0,0 +1,9 @@
+public class ScopeDemo5 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+//        System.out.println(i);
+        // it doesn't work ... -> only in the loop function
+    }
+}
---
+++ javatutorials2/ScopeDemo6.java
@@ -0,0 +1,17 @@
+/*
+    static scope, lexical scope
+ */
+public class ScopeDemo6 {
+    static int i = 5;
+
+    static void a() {
+        int i = 10;
+        b(); // call i -> class (not method)
+    }
+    static void b() {
+        System.out.println(i);
+    }
+    public static void main(String[] args) {
+        a(); // call b
+    }
+}
