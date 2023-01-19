8ab2484fa63d311ed0e385f844c4cc3c5ee75e34
박민주 <parkminjoo@bagminjuui-MacBookPro.local>
2023-01-19 13:40:24 +0700

    [UPDATE] CalculatorDemo2 in Inheritance

--- javatutorials2/Inheritance/CalculatorDemo2.java
+++ javatutorials2/Inheritance/CalculatorDemo2.java
@@ -1,4 +1,19 @@
 package Inheritance;
+/*
+    Another Class to inheritance Calculator
+ */
 
+class MultiplicationableCalculator extends Calculator {
+    public void multiplication() {
+        System.out.println(this.left * this.right);
+    }
+}
 public class CalculatorDemo2 {
+    public static void main(String[] args) {
+        MultiplicationableCalculator c1 = new MultiplicationableCalculator();
+        c1.setOprands(10,20);
+        c1.sum();
+        c1.avg();
+        c1.multiplication();
+    }
 }
