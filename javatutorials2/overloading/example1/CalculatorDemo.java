8640679969c71f41cb3a45f093d1dc6ea3daf359
Park-Minjoo <alswn9030@gmail.com>
2023-01-24 16:41:11 +0700

    [UPDATE] some updates

--- javatutorials2/overloading/example1/CalculatorDemo.java
+++ javatutorials2/overloading/example1/CalculatorDemo.java
@@ -1,4 +1,14 @@
 package overloading.example1;
 
+class Calculator{
+    int left, right;
+    int third = 0;
+
+    public void setOprands(int left, int right) {
+        System.out.println("setOprands(int left, int right)");
+        this.left = left;
+        this.right = right;
+    }
+}
 public class CalculatorDemo {
 }
