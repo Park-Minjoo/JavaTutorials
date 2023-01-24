d126bde4ff16bfc49873742e5ed974fab2aad4dd
Park-Minjoo <alswn9030@gmail.com>
2023-01-24 17:04:29 +0700

    [UPDATE] Some description

--- javatutorials2/overloading/example1/OverloadingDemo2.java
+++ javatutorials2/overloading/example1/OverloadingDemo2.java
@@ -3,8 +3,17 @@ package overloading.example1;
     Can overloading be used in the relationship of inheritance? 'Of Course'
  */
 public class OverloadingDemo2 extends OverloadingDemo{
+    // Class OverloadingDemo2 inherits the OverloadingDemo.
+
     void A(String arg1, String arg2){System.out.println("sub class: void A (String arg1, String arg2)");}
+    // Two parameters with strings as data types
+    // Overloading -> it is not defined in the parent class
+
     void A (){System.out.println("sub class: void A");}
+    // no parameters
+    // Overriding -> method A already exists in the parent class without parameters.
+    // Overloading (X) -> paramters are the same...
+
     public static void main(String[] args){
         OverloadingDemo2 od = new OverloadingDemo2();
         od.A();
