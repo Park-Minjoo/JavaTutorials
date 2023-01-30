cd2ef62f6647c1a0a635072f04ef3f3c703db40f
Park-Minjoo <alswn9030@gmail.com>
2023-01-30 15:19:12 +0700

    [UPDATE] Package end

--- javatutorials2/packages/example2/C.java
+++ javatutorials2/packages/example2/C.java
@@ -3,6 +3,6 @@ import packages.example1.*; // Every class in example1 package
 
 public class C {
     public static void main(String[] args) {
-        A a new A();
+        A a = new A();
     }
 }
---
+++ javatutorials2/packages/example3/D.java
@@ -0,0 +1,9 @@
+package packages.example3;
+
+import packages.example1.*;
+import packages.example2.*;
+public class D {
+    public static void main(String[] args) {
+        packages.example2.B b = new packages.example2.B();
+    }
+}
