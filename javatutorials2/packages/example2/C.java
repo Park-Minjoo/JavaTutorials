f6382811b8093e1c279aae3c080b7a7fe8b516bb
Park-Minjoo <alswn9030@gmail.com>
2023-01-30 15:16:14 +0700

    [UPDATE] package example2 B

---
+++ javatutorials2/packages/example2/C.java
@@ -0,0 +1,8 @@
+package packages.example2;
+import packages.example1.*; // Every class in example1 package
+
+public class C {
+    public static void main(String[] args) {
+        A a new A();
+    }
+}
