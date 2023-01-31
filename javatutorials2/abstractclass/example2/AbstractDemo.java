18799d76788bff87129e54ea665725604b3be21f
Park-Minjoo <alswn9030@gmail.com>
2023-01-31 16:11:28 +0700

    [UPDATE] Abstract Class - example2

--- javatutorials2/abstractclass/example2/AbstractDemo.java
+++ javatutorials2/abstractclass/example2/AbstractDemo.java
@@ -1,4 +1,17 @@
 package abstractclass.example2;
 
+abstract class A{
+    public abstract int b();
+    public void d(){
+        System.out.println("world");
+    }
+}
+class B extends A{ /* Inheritance */
+    public int b(){return 1;} /* Overriding */
+}
 public class AbstractDemo {
+    public static void main(String[] args) {
+        B obj = new B();
+        System.out.println(obj.b());
+    }
 }
