520b5224284b091f672df368b82a973df3b3becf
Park-Minjoo <alswn9030@gmail.com>
2023-02-15 11:09:25 +0700

    [UPDATE] Overloading and Polymorphism

---
+++ javatutorials2/polymorphism/PolymorphismOverloadingDemo.java
@@ -0,0 +1,21 @@
+package polymorphism;
+/*
+    Overloading and Polymorphism
+ */
+class O{
+    public void a(int param){
+        System.out.println("Print the number");
+        System.out.println(param);
+    }
+    public void a(String param){
+        System.out.println("Print the number");
+        System.out.println(param);
+    }
+}
+public class PolymorphismOverloadingDemo {
+    public static void main(String[] args) {
+        O o = new O();
+        O.a(1);
+        o.a("one");
+    }
+}
