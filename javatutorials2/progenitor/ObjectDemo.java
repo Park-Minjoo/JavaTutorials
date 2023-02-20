d91831334b9710147a96c7384ab7ca5000843d6a
Park-Minjoo <alswn9030@gmail.com>
2023-02-20 09:54:33 +0700

    [UPDATE] progenitor - Clone

--- javatutorials2/progenitor/ObjectDemo.java
+++ javatutorials2/progenitor/ObjectDemo.java
@@ -1,20 +1,30 @@
 package progenitor;
 
-class Student{
+class Student implements Cloneable{
     String name;
     Student(String name){
         this.name = name;
     }
-    public boolean equals(Object obj){
-        Student _obj = (Student) obj;
-        return  name == _obj.name;
+//    public boolean equals(Object obj){
+//        Student _obj = (Student) obj;
+//        return  name == _obj.name;
+//    }
+    protected Object clone() throws CloneNotSupportedException{
+        return super.clone();
     }
 }
 class ObjectDemo {
     public static void main(String[] args) {
         Student s1 = new Student("egoing"); // false
-        Student s2 = new Student("egoing"); // true
-        System.out.println(s1 == s2);
-        System.out.println(s1.equals(s2));
+//        Student s2 = new Student("egoing"); // true
+//        System.out.println(s1 == s2);
+//        System.out.println(s1.equals(s2));
+        try {
+            Student s2 = (Student) s1.clone();
+            System.out.println(s1.name);
+            System.out.println(s2.name);
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
     }
 }
