b3ea54c92e9ecbe5aa0b1a9211a86ab33e4e68c7
Park-Minjoo <alswn9030@gmail.com>
2023-02-20 11:10:48 +0700

    [UPDATE] namespace

--- javatutorials2/constant2/ConstantDemo.java
+++ javatutorials2/constant2/ConstantDemo.java
@@ -1,9 +1,15 @@
 package constant2;
 
 public class ConstantDemo {
-    private final static int APPLE = 1;
-    private final static int PEACH = 2;
-    private final static int BANANA = 3;
+    // fruit
+    private final static int FRUIT_APPLE = 1;
+    private final static int FRUIT_PEACH = 2;
+    private final static int FRUIT_BANANA = 3;
+
+    // company
+    private final static int COMPANY_GOOGLE = 1;
+    private final static int COMPANY_APPLE = 2;
+    private final static int COMPANY_ORACLE = 3;
 
     public static void main(String[] args) {
         /*
@@ -11,15 +17,15 @@ public class ConstantDemo {
          * 2. 복숭아
          * 3. 바나나
          */
-        int type = 1;
+        int type = FRUIT_APPLE;
         switch(type){
-            case APPLE:
+            case FRUIT_APPLE:
                 System.out.println(57+" kcal");
                 break;
-            case PEACH:
+            case FRUIT_PEACH:
                 System.out.println(34+" kcal");
                 break;
-            case BANANA:
+            case FRUIT_BANANA:
                 System.out.println(93+" kcal");
                 break;
         }
