5d993fb48019b1af9ec2c7a16fa5d56fc10bcec2
Park-Minjoo <alswn9030@gmail.com>
2023-02-20 11:33:53 +0700

    [UPDATE] runValue & runRefernce

--- javatutorials2/reference/ReferenceDemo1_1.java
+++ javatutorials2/reference/ReferenceDemo1_1.java
@@ -1,4 +1,26 @@
 package reference;
-class A
+class A{
+    public int id;
+    A(int id){
+        this.id = id;
+    }
+}
 public class ReferenceDemo1_1 {
+    public static void runValue(){
+        int a = 1;
+        int b = a;
+        b = 2;
+        System.out.println("runValue, "+a);
+    }
+    public static void runReference(){
+        A a = new A(1);
+        A b = a;
+        b.id = 2;
+        System.out.println("runReference, "+a.id);
+    }
+
+    public static void main(String[] args) {
+        runValue();
+        runReference();
+    }
 }
