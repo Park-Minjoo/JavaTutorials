da500ced78b4c0eedbd40ca9ef5f4f4cffe49403
Park-Minjoo <alswn9030@gmail.com>
2023-02-20 13:46:33 +0700

    [UPDATE] generic

--- javatutorials2/generic/GenericDemo2.java
+++ javatutorials2/generic/GenericDemo2.java
@@ -1,27 +1,42 @@
 package generic;
-
-class EmployeeInfo{
+//abstract class Info{
+//    public abstract int getLevel();
+//}
+interface Info{
+    int getLevel();
+}
+class EmployeeInfo implements Info{
     public int rank;
     EmployeeInfo(int rank){ this.rank = rank; }
+    public int getLevel(){
+        return this.rank;
+    }
 }
-class Person<T, S>{
+class Person<T extends Info>{
     public T info;
-    public S id;
-    Person(T info, S id){
-        this.info = info;
-        this.id = id;
-    }
-    public <U> void printInfo(U info){
-        System.out.println(info);
-    }
+    Person(T info) { this.info = info; }
+//    public S id;
+//    Person(T info, S id){
+//        this.info = info;
+//        this.id = id;
+//    }
+//    public <U> void printInfo(U info){
+//        System.out.println(info);
+//    }
+}
+class Person<T extends Info>{
+    public T info;
+    Person(T info){ this.info = info; }
 }
 public class GenericDemo2 {
     public static void main(String[] args) {
-        EmployeeInfo e = new EmployeeInfo(1);
-        Integer i = new Integer(10);
-        Person<EmployeeInfo, Integer> p1 = new Person<EmployeeInfo, Integer>(new EmployeeInfo(1), 1);
-        p1.<EmployeeInfo>printInfo(e);
+//        EmployeeInfo e = new EmployeeInfo(1);
+//        Integer i = new Integer(10);
+//        Person<EmployeeInfo, Integer> p1 = new Person<EmployeeInfo, Integer>(new EmployeeInfo(1), 1);
+//        p1.<EmployeeInfo>printInfo(e);
 //        System.out.println(p1.id.intValue());
-        p1.printInfo(e);
+//        p1.printInfo(e);
+        Person p1 = new Person(new EmployeeInfo(1));
+        Person<String> p2 = new Person<String>("부장")
     }
 }
