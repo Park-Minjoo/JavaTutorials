547fe8432a21c9506bfddba837ce023ff0339a48
Park-Minjoo <alswn9030@gmail.com>
2023-02-20 11:53:54 +0700

    [UPDATE] Generic

---
+++ javatutorials2/generic/GenericDemo.java
@@ -0,0 +1,11 @@
+package generic;
+
+class Person<T>{
+    public T info;
+}
+public class GenericDemo {
+    public static void main(String[] args) {
+        Person<String> p1 = new Person<String>();
+        Person<StringBuilder> p2 = new Person<StringBuilder>();
+    }
+}
