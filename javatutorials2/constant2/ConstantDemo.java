7be2800b9c8ccdbc803a523d47e3bdeba55e8493
Park-Minjoo <alswn9030@gmail.com>
2023-02-20 11:12:47 +0700

    [UPDATE] interface"

--- javatutorials2/constant2/ConstantDemo.java
+++ javatutorials2/constant2/ConstantDemo.java
@@ -1,31 +1,36 @@
 package constant2;
 
-public class ConstantDemo {
     // fruit
-    private final static int FRUIT_APPLE = 1;
-    private final static int FRUIT_PEACH = 2;
-    private final static int FRUIT_BANANA = 3;
-
-    // company
-    private final static int COMPANY_GOOGLE = 1;
-    private final static int COMPANY_APPLE = 2;
-    private final static int COMPANY_ORACLE = 3;
-
+//    private final static int FRUIT_APPLE = 1;
+//    private final static int FRUIT_PEACH = 2;
+//    private final static int FRUIT_BANANA = 3;
+//
+//    // company
+//    private final static int COMPANY_GOOGLE = 1;
+//    private final static int COMPANY_APPLE = 2;
+//    private final static int COMPANY_ORACLE = 3;
+interface FRUIT{
+    int APPLE = 1, PEACH = 2, BANANA = 3;
+}
+interface COMPANY{
+    int GOOGLE = 1, APPLE = 2, ORACLE = 3;
+}
+public class ConstantDemo {
     public static void main(String[] args) {
         /*
          * 1. 사과
          * 2. 복숭아
          * 3. 바나나
          */
-        int type = FRUIT_APPLE;
+        int type = FRUIT.APPLE;
         switch(type){
-            case FRUIT_APPLE:
+            case FRUIT.APPLE:
                 System.out.println(57+" kcal");
                 break;
-            case FRUIT_PEACH:
+            case FRUIT.PEACH:
                 System.out.println(34+" kcal");
                 break;
-            case FRUIT_BANANA:
+            case FRUIT.BANANA:
                 System.out.println(93+" kcal");
                 break;
         }
