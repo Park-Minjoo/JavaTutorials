5878b015f80191f5295465e45ecaeb3731d9a5e1
Park-Minjoo <alswn9030@gmail.com>
2023-02-20 11:17:11 +0700

    [UPDATE] CLass and instance

--- javatutorials2/constant2/ConstantDemo.java
+++ javatutorials2/constant2/ConstantDemo.java
@@ -9,11 +9,23 @@ package constant2;
 //    private final static int COMPANY_GOOGLE = 1;
 //    private final static int COMPANY_APPLE = 2;
 //    private final static int COMPANY_ORACLE = 3;
-interface FRUIT{
-    int APPLE = 1, PEACH = 2, BANANA = 3;
-}
-interface COMPANY{
-    int GOOGLE = 1, APPLE = 2, ORACLE = 3;
+//interface FRUIT{
+//    int APPLE = 1, PEACH = 2, BANANA = 3;
+//}
+//interface COMPANY{
+//    int GOOGLE = 1, APPLE = 2, ORACLE = 3;
+//}
+class Fruit{
+    public static final Fruit APPLE = new Fruit();
+    public static final Fruit PEACH = new Fruit();
+    public static final Fruit BANANA = new Fruit();
+
+    }
+class Company{
+    public static final Company GOOGLE = new Company();
+    public static final Company APPLE = new Company();
+    public static final Company ORACLE = new COMPANY(Company);
+
 }
 public class ConstantDemo {
     public static void main(String[] args) {
@@ -22,17 +34,19 @@ public class ConstantDemo {
          * 2. 복숭아
          * 3. 바나나
          */
-        int type = FRUIT.APPLE;
-        switch(type){
-            case FRUIT.APPLE:
-                System.out.println(57+" kcal");
-                break;
-            case FRUIT.PEACH:
-                System.out.println(34+" kcal");
-                break;
-            case FRUIT.BANANA:
-                System.out.println(93+" kcal");
-                break;
+//        int type = COMPANY.GOOGLE;
+//        switch(type){
+//            case FRUIT.APPLE:
+//                System.out.println(57+" kcal");
+//                break;
+//            case FRUIT.PEACH:
+//                System.out.println(34+" kcal");
+//                break;
+//            case FRUIT.BANANA:
+//                System.out.println(93+" kcal");
+//                break;
+        if(Fruit.APPLE == Company.APPLE){
+            System.out.println("과일 애플과 회사 애풀이 같다.");
         }
     }
 }
