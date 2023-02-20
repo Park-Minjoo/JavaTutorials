8e6dd73faaaceb3862afe2e2e2c62c38c4755d74
Park-Minjoo <alswn9030@gmail.com>
2023-02-20 11:00:11 +0700

    [UPDATE] Constant2

---
+++ javatutorials2/constant2/ConstantDemo.java
@@ -0,0 +1,23 @@
+package constant2;
+
+public class ConstantDemo {
+    public static void main(String[] args) {
+        /*
+         * 1. 사과
+         * 2. 복숭아
+         * 3. 바나나
+         */
+        int type = 1;
+        switch(type){
+            case 1:
+                System.out.println(57);
+                break;
+            case 2:
+                System.out.println(34);
+                break;
+            case 3:
+                System.out.println(93);
+                break;
+        }
+    }
+}
