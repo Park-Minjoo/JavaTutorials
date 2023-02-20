0ca694b9476b5ac69007fe5a0b4c2d8163b72e30
Park-Minjoo <alswn9030@gmail.com>
2023-02-20 11:20:24 +0700

    [UPDATE] enum

--- javatutorials2/constant2/ConstantDemo.java
+++ javatutorials2/constant2/ConstantDemo.java
@@ -15,17 +15,23 @@ package constant2;
 //interface COMPANY{
 //    int GOOGLE = 1, APPLE = 2, ORACLE = 3;
 //}
-class Fruit{
-    public static final Fruit APPLE = new Fruit();
-    public static final Fruit PEACH = new Fruit();
-    public static final Fruit BANANA = new Fruit();
-
-    }
-class Company{
-    public static final Company GOOGLE = new Company();
-    public static final Company APPLE = new Company();
-    public static final Company ORACLE = new COMPANY(Company);
-
+//class Fruit{
+//    public static final Fruit APPLE = new Fruit();
+//    public static final Fruit PEACH = new Fruit();
+//    public static final Fruit BANANA = new Fruit();
+//
+//    }
+//class Company{
+//    public static final Company GOOGLE = new Company();
+//    public static final Company APPLE = new Company();
+//    public static final Company ORACLE = new COMPANY(Company);
+//
+//}
+enum Fruit{
+    APPLE, PEACH, BANANA;
+}
+enum Company{
+    GOOGLE, APPLE, ORACLE;
 }
 public class ConstantDemo {
     public static void main(String[] args) {
@@ -45,8 +51,22 @@ public class ConstantDemo {
 //            case FRUIT.BANANA:
 //                System.out.println(93+" kcal");
 //                break;
+        /*
         if(Fruit.APPLE == Company.APPLE){
             System.out.println("과일 애플과 회사 애풀이 같다.");
+        }
+         */
+        Fruit type = Fruit.APPLE;
+        switch (type){
+            case APPLE:
+                System.out.println(57 + " kcal");
+                break;
+            case PEACH:
+                System.out.println(34 + " kcal");
+                break;
+            case BANANA:
+                System.out.println(93 + " kcal");
+                break;
         }
     }
 }
